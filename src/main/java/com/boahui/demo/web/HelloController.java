874acd3463e56d2b38f6b296ace16f14a21f1787
wangbaohui <13802885224@139.com>
2023-01-29 16:44:13 +0800

    服务中心的使用

--- src/main/java/com/boahui/demo/web/HelloController.java
+++ src/main/java/com/boahui/demo/web/HelloController.java
@@ -1,12 +1,27 @@
 package com.boahui.demo.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String index(){
-        return  "Hello World";
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        client.getServices().forEach(id -> {
+            client.getInstances(id).forEach(instanc -> {
+                logger.info("/hello,host:" + instanc.getHost() + ",serivce_id:" + instanc.getServiceId());
+            });
+        });
+        return "Hello World";
     }
 }
